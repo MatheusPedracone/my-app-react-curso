97e7401ea63353019dccec18215d50cc8e29db9d
Matheus <matheuspedracone@gmail.com>
2022-08-22 20:18:20 -0300

    feat: Adding Dark Theme and change themes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@mui/material";
 import { Routes, Navigate, Route } from "react-router-dom";
+import { useAppThemeContext } from "../shared/context";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="dashboard" element={<Button variant="contained" color="primary">TESTE</Button>} />
+      <Route path="dashboard" element={<Button variant="contained" color="primary" onClick={toggleTheme}>toggle theme</Button>} />
 
       <Route path="*" element={<Navigate to="/dashboard" />} />
     </Routes>
