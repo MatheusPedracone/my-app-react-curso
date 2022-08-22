aa1017afb828344d387f167a3fb742fc8d15015d
Matheus <matheuspedracone@gmail.com>
2022-08-22 17:52:17 -0300

    feat: Adding Light theme and providers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Navigate, Route } from "react-router-dom";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="dashboard" element={<Button>TESTE</Button>} />
+      <Route path="dashboard" element={<Button variant="contained" color="primary">TESTE</Button>} />
 
       <Route path="*" element={<Navigate to="/dashboard" />} />
     </Routes>
